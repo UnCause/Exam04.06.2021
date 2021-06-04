e5119b7806ee1f72b2f8d590ca1de622242318aa
UnCause <uncausedisskous@gmail.com>
2021-06-04 09:46:28 +0300

    Release

---
+++ Exam/Exam/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Exam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Primes primes = new Primes(0, 100);
+            primes.Start();
+        }
+    }
+}
